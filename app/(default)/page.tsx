d546dae279d785776a9968859a5a8a830ce84d49
lxj <1851816672@qq.com>
2024-06-10 00:53:16 +0800

    [update]update

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -2,7 +2,7 @@
  * @Author: lxj 1851816672@qq.com
  * @Date: 2024-06-06 18:37:22
  * @LastEditors: lxj 1851816672@qq.com
- * @LastEditTime: 2024-06-10 00:44:43
+ * @LastEditTime: 2024-06-10 00:49:52
  * @FilePath: /FuelCN/app/(default)/page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,11 +22,6 @@ export default function Home() {
   return (
     <>
       <Articallist />
-      {/* <Hero />
-      <Features />
-      <FeaturesBlocks />
-      <Testimonials />
-      <Newsletter /> */}
     </>
   )
 }
